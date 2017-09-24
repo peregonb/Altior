afab18ecba2fafdf79029a178179b738e19e5b82
Bogdan Peregon <peregonbw@gmail.com>
2017-09-24 18:42:33 +0300

    v1

---
+++ app/js/common.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+  $(".hamburger").click(function(){
+    $(this).toggleClass("is-active");
+  });
+});
+$(function(){
+ var shrinkHeader = 300;
+  $(window).scroll(function() {
+    var scroll = getCurrentScroll();
+      if ( scroll >= shrinkHeader ) {
+           $('.header').addClass('shrink');
+        }
+        else {
+            $('.header').removeClass('shrink');
+        }
+  });
+function getCurrentScroll() {
+    return window.pageYOffset || document.documentElement.scrollTop;
+    }
+});
