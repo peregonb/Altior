9eae53890b22fb5f525bd1b26016ed672ba521d3
Bogdan Peregon <peregonbw@gmail.com>
2017-09-27 18:07:53 +0300

    v2

--- app/js/common.js
+++ app/js/common.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
+  $('.main-menu').hide();
   $(".hamburger").click(function(){
+    if (!$('.header').hasClass('shrink')) {
+    $('.header').toggleClass('shrink');
+      
+    };
     $(this).toggleClass("is-active");
+    // $('.main-menu').slideToggle({ left: 'toggle' });
+    $('.main-menu').animate({'width': 'toggle'}, 300);
+    // $('.first-menu-item').css("margin-top", 60);
   });
 });
 $(function(){
@@ -19,3 +27,71 @@ function getCurrentScroll() {
     }
 
 });
+
+$(document).ready(function(){
+        $(window).scroll(function footerPosition(){
+var Fh = $('.footer').height();
+var AFh = $('.afterfooter').height();
+var Bh = $('body').height();
+var Bh2 = $(window).outerHeight();
+var Hh = $('.header').height();
+var Fw = $('.footer-wrapper').height();
+var AllFh = Fh + AFh;
+var bo = $(window).scrollTop();
+console.log(bo , Bh2);
+if (bo > Bh - Bh2 - AllFh - Hh) {
+  $('.index-button-stack').removeAttr('style');
+  $('.index-button-stack').css("bottom", 20);
+  $('.index-button-stack').css("position", "absolute").height(56);
+}
+else{
+  $('.index-button-stack').css("top", bo + Bh2 - 80);
+   $('.index-button-stack').css("position", "absolute").height(56);
+}
+        });
+        var Fh = $('.footer').height();
+var AFh = $('.afterfooter').height();
+var Bh = $('body').height();
+var Bh2 = $(window).outerHeight();
+var Hh = $('.header').height();
+var Fw = $('.footer-wrapper').height();
+var AllFh = Fh + AFh;
+var bo = $(window).scrollTop();
+console.log(bo , Bh2);
+if (bo > Bh - Bh2 - AllFh - Hh) {
+  $('.index-button-stack').removeAttr('style');
+  $('.index-button-stack').css("bottom", 20);
+  $('.index-button-stack').css("position", "absolute").height(56);
+}
+else{
+  $('.index-button-stack').css("top", bo + Bh2 - 80);
+   $('.index-button-stack').css("position", "absolute").height(56);}
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// $(window).resize(function() {
+//         var winwidth = $(window).outerWidth();
+//         if (winwidth > 991) {
+
+//         } else if (winwidth < 992) {
+
+//         }
+// var Fh = $('.footer').height();
+// var AFh = $('.afterfooter').height();
+// var AllFh = Fh + AFh;
+// console.log(Bh - AllFh);
+//     });
